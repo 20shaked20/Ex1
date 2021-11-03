acc67125ad9de977977636e8788b1b8a494e9edd
Shaked Levi <73894107+20shaked20@users.noreply.github.com>
2021-11-03 15:13:27 +0200

    Update main.c

--- Files/main.c
+++ Files/main.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 #include "NumClass.h"
 
@@ -65,4 +66,46 @@ int main(){
     }
     printf("\n");
     return 0;
+}
+*/
+#include <stdio.h>
+#include "NumClass.h"
+int main(){
+int a;
+int b;
+scanf("%d%d",&a,&b);
+if(a<1){
+    a=1;
+}else if(b<1){
+    b=1;
+}
+printf("The Armstrong numbers are:");
+for(int i =a;i<=b;i++){
+    if(isArmstrong(i)){
+        printf(" %d",i);
+    }
+}
+printf("\n");
+    printf("The Palindromes are:");
+for(int i =a;i<=b;i++){
+    if(isPalindrome(i)){
+        printf(" %d",i);
+    }
+}
+printf("\n");
+    printf("The Prime numbers are:");
+for(int i =a;i<=b;i++){
+    if(isPrime(i)){
+        printf(" %d",i);
+    }
+}
+printf("\n");
+    printf("The Strong numbers are:");
+for(int i =a;i<=b;i++){
+    if(isStrong(i)){
+        printf(" %d",i);
+    }
+}
+printf("\n");
+return 0;
 }

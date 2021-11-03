775c91b1074f9f176d455c9fdf909d1cb81c51f3
Shaked Levi <73894107+20shaked20@users.noreply.github.com>
2021-11-03 12:39:19 +0200

    Add files via upload
    updates to try and fit in python runner

--- Files/main.c
+++ Files/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "NumClass.h"
-#include <math.h>
 
-int main(void){
+int main(int num1,int num2){
+   /*
     int num1;
     int num2;
     printf("Enter first positive number: ");
+    */
     scanf("%d",&num1);
-    printf("Enter a second positive number: ");
+   // printf("Enter a second positive number: ");
     scanf("%d",&num2);
     int max,min;
     if(num1>num2){
@@ -17,37 +18,52 @@ int main(void){
         max = num2;
         min = num1;
     }
-     printf("The Armstrong numbers are: ");
+     printf("The Armstrong numbers are:");
     for(int i = min; i<=max; i++){
+        if(i == 0){
+            i++;
+        }
         if(isArmstrong(i)==1){
-            printf("%d, ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
 
-    printf("The Palindrome numbers are: ");
+    printf("The Palindromes are:");
     for(int i = min; i<=max; i++){
+        if(i == 0){
+            i++;
+        }
         if(isPalindrome(i)==1){
-            printf("%d, ", i);
+            printf(" %d", i);
         }
     }
     
     printf("\n");
 
-    printf("The Prime numbers are: ");
+    printf("The Prime numbers are:");
     for(int i = min; i<=max; i++){
+        if(i == 0){
+            i++;
+        }
+        if(i == 1){
+            printf(" %d", i); // i = 1 is prime so print..
+        }
         if(isPrime(i)==1){
-            printf("%d, ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
 
-     printf("The Strong numbers are: ");
+     printf("The Strong numbers are:");
     for(int i = min; i<=max; i++){
+        if(i == 0){
+            i++;
+        }
         if(isStrong(i)==1){
-            printf("%d, ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
- 
+    return 0;
 }
